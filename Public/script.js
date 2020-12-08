c8674b882dea50480cac42ed3804a578767ff851
ArktheLass <arkpeggs@gmail.com>
2020-12-07 19:10:33 -0600

    dash

---
+++ Public/script.js
@@ -0,0 +1,36 @@
+
+        $("#newProject") .mouseover(function () {
+            this.src= "newProject-hover.png"
+         }).mouseout(function () {
+             this.src= "newProject.png"
+         });
+         
+        $("#continueProject") .mouseover(function () {
+            this.src= "continue-hover.png"
+         }).mouseout(function () {
+             this.src= "continue.png"
+         });
+         
+        $("#customizeAvatar") .mouseover(function () {
+            this.src= "customize-hover.png"
+         }).mouseout(function () {
+             this.src= "customize.png"
+         });
+         
+        $("#shoppe") .mouseover(function () {
+            this.src= "shop-hover.png"
+         }).mouseout(function () {
+             this.src= "shop.png"
+         });
+         
+        $("#partyQuest") .mouseover(function () {
+            this.src= "party-quest-hover.png"
+         }).mouseout(function () {
+             this.src= "party-quest.png"
+         });
+         
+        $("#logout") .mouseover(function () {
+            this.src= "sign-out-hover.png"
+         }).mouseout(function () {
+             this.src= "sign-out.png"
+         });
